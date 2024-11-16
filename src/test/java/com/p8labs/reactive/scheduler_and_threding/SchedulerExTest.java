60bb9ce0656e0baf03c510ae141535c9d2e90274
skahn <d1215@naver.com>
2024-11-17 00:39:05 +0900

    feat: Add example scheduler

---
+++ src/test/java/com/p8labs/reactive/scheduler_and_threding/SchedulerExTest.java
@@ -0,0 +1,14 @@
+package com.p8labs.reactive.scheduler_and_threding;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SchedulerExTest {
+    private SchedulerEx ex = new SchedulerEx();
+
+    @Test
+    void test() {
+        ex.scheduler();
+    }
+}
