0d6aa5dadf4282a42c7a903b5df4bfc97dde1826
skahn <d1215@naver.com>
2025-01-11 18:37:22 +0900

    feat: Add example scheduler

--- src/main/java/com/p8labs/reactive/scheduler_and_threding/SchedulerEx.java
+++ src/main/java/com/p8labs/reactive/scheduler_and_threding/SchedulerEx.java
@@ -1,15 +1,18 @@
 package com.p8labs.reactive.scheduler_and_threding;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
+@Slf4j
 public class SchedulerEx {
 
     public void scheduler() throws InterruptedException {
         Scheduler scheduler = Schedulers.newParallel("parallel-scheduler", 4);
 
+        log.info("Scheduler started");
         Flux<String> flux = Flux
                 .range(1, 2)
                 .map(i -> {
@@ -25,8 +28,26 @@ public class SchedulerEx {
                 .subscribeOn(scheduler)
                 .map(i -> "value " + i);
 
-        flux.subscribe(System.out::println);
 
+        Flux<String> flux2 = Flux
+                .range(1, 2)
+                .map(i -> {
+
+                    try {
+                        Thread.sleep(1000);
+                    }catch (Exception e) {
+
+                    }
+                    return 20 + i;
+
+                })
+                .subscribeOn(scheduler)
+                .map(i -> "2-value " + i);
+
+        flux.subscribe(System.out::println);
+        flux2.subscribe(System.out::println);
+        flux2.zipWith(flux).subscribe(System.out::println);
+        log.info("Scheduler end");
         Thread.sleep(5000);
 
     }
